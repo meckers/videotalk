ad75469b1418b075d555485dfd454b909fdd9708
Magnus Eckers <magnus.eckers@hitta.se>
2013-09-27 14:38:56 +0200

    Annotations working for the first time. More work to be done.

--- public/javascripts/lab1.js
+++ public/javascripts/lab1.js
@@ -106,12 +106,47 @@ YouTubeAction = Action.extend({
         var me = this;
         console.log("video now playing");
         this.pollInterval = window.setInterval(function() {
-            console.log("Polling current time (seconds)", me.player.getCurrentTime());
+            me.onPoll();
         }, 500);
     },
     onStop: function() {
         window.clearInterval(this.pollInterval);
         console.log("video stopped/paused");
+    },
+    onPoll: function() {
+        var time = this.player.getCurrentTime();
+        this.currentAnnotation = this.getAnnotation(time);
+
+        if (this.currentAnnotation) {
+            if ($("#youtube-annotation:visible").length === 0) {
+                this.showAnnotation(this.currentAnnotation.text);
+            }
+        }
+        else {
+            if ($("#youtube-annotation:visible").length > 0) {
+                this.hideAnnotation();
+            }
+        }
+    },
+    getAnnotation: function(time) {
+        var validAnnotations = $.grep(this.data.annotations, function(n) {
+            console.log(n.start, time, (n.start < time && (n.start + n.duration) > time), "end", n.start + n.duration);
+            return (n.start < time && (n.start + n.duration) > time);
+        })
+        if (validAnnotations.length > 0) {
+            return validAnnotations[0];
+        }
+        return null;
+    },
+    showAnnotation: function(text) {
+        console.log("showing annotation", text);
+        this.textElement = $("<div></div>");
+        this.textElement.attr('id', 'youtube-annotation');
+        this.textElement.html(text);
+        $("#lab1-stage").append(this.textElement);
+    },
+    hideAnnotation: function() {
+        this.textElement.remove();
     }
 });
 
@@ -179,13 +214,13 @@ var G = {
             'chardelay': 100
         }), */
         new YouTubeAction({
-            'container': '#lab1-stage',
+            'container': '#youtube-video',
             'data': {
                 'start': 10,
                 'url': 'http://www.youtube.com/v/ii4Ev8Dyo20',
                 'annotations': [{
-                    'start': 5,
-                    'duration': 2,
+                    'start': 2,
+                    'duration': 5,
                     'text': 'This video is WHACK!'
                 }]
             },
